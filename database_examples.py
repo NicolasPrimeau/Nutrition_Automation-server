a03f87c6f32c4e21c393be486bc43636b4b37a78
Nicolas Primeau <nicolas.primeau@gmail.com>
2014-12-01 14:05:50 -0500

    last minute changes

--- database_examples.py
+++ database_examples.py
@@ -15,15 +15,15 @@ alarm = {}
 alarm['description'] = "General catch all alarm for testing"
 alarm['type'] = "quantity"
 alarm['flag'] = {}
-alarm['flag']['max'] = 100
-alarm['flag']['min'] = 20
+alarm['flag']['max'] = 100.0
+alarm['flag']['min'] = 20.0
 alarm['target_bins'] = []
 alarm['target_bins'] = -1
 alarm['contact'] = []
 c = {}
 c['name'] = "Nicolas Primeau"
 c['email'] = True
-c['phone'] = False
+c['phone'] = True
 
 alarm['contact'].append(c)
 
@@ -33,10 +33,10 @@ import datetime
 
 food = {}
 food['bin'] = 1
-food['quantity'] = 19
+food['quantity'] = 19.8
 food['date'] = datetime.datetime.now()
 
-database_interface.store_data(database_interface.FOOD, food)
+#database_interface.store_data(database_interface.FOOD, food)
 
 config = {}
 config['bin'] = 1
@@ -46,6 +46,13 @@ config['perishable'] = True
 
 #database_interface.store_data(database_interface.CONFIG, config)
 
+config={}
+config['bin'] = 2
+config['name'] = "Banana"
+config['type'] = "Fruit"
+config['perishable'] = True
+
+#database_interface.store_data(database_interface.CONFIG, config)
 
 
 

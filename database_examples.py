94076709a20c0e7c1468427d6341e32f701208c4
Nixon- <nicolas.primeau@gmail.com>
2015-01-17 12:28:47 -0500

    fixes, testing and general rebriefing of project since long break

--- database_examples.py
+++ database_examples.py
@@ -9,7 +9,7 @@ contact['phone'] = "6139157187"
 contact['email'] = "nicolas.primeau@gmail.com"
 
 #store data
-database_interface.store_data(database_interface.CONTACT,contact)
+#database_interface.store_data(database_interface.CONTACT,contact)
 
 alarm = {}
 alarm['description'] = "General catch all alarm for testing"
@@ -17,8 +17,7 @@ alarm['type'] = "quantity"
 alarm['flag'] = {}
 alarm['flag']['max'] = 100.0
 alarm['flag']['min'] = 20.0
-alarm['target_bins'] = []
-alarm['target_bins'] = -1
+alarm['target_bins'] = [1,2]
 alarm['contact'] = []
 c = {}
 c['name'] = "Nicolas Primeau"
@@ -27,13 +26,13 @@ c['phone'] = True
 
 alarm['contact'].append(c)
 
-database_interface.store_data(database_interface.ALERT, alarm)
+#database_interface.store_data(database_interface.ALERT, alarm)
 
 import datetime
 
 food = {}
-food['bin'] = 1
-food['quantity'] = 19.8
+food['bin'] = 2
+food['quantity'] = 21.0
 food['date'] = datetime.datetime.now()
 
 database_interface.store_data(database_interface.FOOD, food)
--- test-shell.py
+++ test-shell.py
@@ -1,56 +1,59 @@
 #!/usr/bin/env python
 
 
-import database_interface
-import info_checking
-import signal
+import database_interface, info_checking, data_input
 from multiprocessing import Process
-import data_input
-import os 
+import os,sys, signal
 
 pids = {}
 
 def main():
 
-  while True:
-    cmd = raw_input(">")
+    signal.signal(signal.SIGINT, exit_handler)
+
+    while True:
+      cmd = raw_input(">")
 
 
-    if cmd.lower() == "exit":
-     break;
+      if cmd.lower() == "exit":
+         break;
   
-    if cmd in cmds:
-      cmds[cmd]()
-    elif cmd in stop_cmds:
-      stop_cmds[cmd]  
-    else:
-      print("Command not supported")
+      if cmd in cmds:
+          cmds[cmd]()
+      elif cmd in stop_cmds:
+          stop_cmds[cmd]  
+      else:
+          print("Command not supported")
 
 def start_tcp_server():
-  print ("Server started")
-  if 'server' not in pids:
-    pids['server'] = Process(target = data_input.startServer)
+    print ("Server started")
+    if 'server' not in pids:
+        pids['server'] = Process(target = data_input.startServer)
 
 
 def cleanup_db():
-  return
+    return
 
 def check_data():
-  info_checking.check_data()
-  return
+    info_checking.check_data()
+    return
 
 def stop_tcp_server():
-  os.kill(pid['server'], signal.SIGINT)  
+    os.kill(pid['server'], signal.SIGINT)  
 
+def exit_handler(signal, frame):
+    print("\nBye\n")
+    sys.exit(0)
 
-
-
+def help_func():
+    print("I'll allow this eventually")
 
 
 cmds = {}
 cmds['start_server'] = start_tcp_server
 cmds['cleanup_db'] = cleanup_db
 cmds['check_data'] = check_data
+cmds['help'] = help_func
 
 stop_cmds = {}
 stop_cmds['stop_server'] = stop_tcp_server
